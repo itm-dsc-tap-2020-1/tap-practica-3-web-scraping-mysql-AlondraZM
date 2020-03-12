44d0becdfaa07806ee54a405865a6173e0ce006a
AlondraZM <61289225+AlondraZM@users.noreply.github.com>
2020-03-12 11:43:13 -0600

    Add files via upload

--- practica3.py
+++ practica3.py
@@ -15,31 +15,50 @@ url = urlopen(pag_inicial)
 
 print("\nENLACES EXTRAIDOS DE LA PAGINA WEB: " + pag_inicial + "\n")
 bs = BeautifulSoup(url.read(), 'html.parser')
-for enlaces in bs.find_all("a"):
-    pag=enlaces.get("href")
-    b=False
-    ##insertar en la tabla
+lista_enlaces=bs.find_all("a")
+for enlaces in lista_enlaces :
+    for i in lista_enlaces:
+        try:
+            url: str=i["href"]
+        except KeyError:
+            continue
+        if not url.startswith("http"):
+            continue
+        try:
+            operacion.execute(f'INSERT INTO web VALUES ("{url}",false)')
+        except mysql.errors.IntegrityError:
+            continue
     print("href: {}".format(enlaces.get("href")))
 print("\nFIN DE ENLACES ENCONTRADOS EN:  "+pag_inicial+"\n")
 #mostrar tabla
 for pagina,status,  in operacion.fetchall() :
     print (pagina,status)
 
-
 for pagina,status,  in operacion.fetchall():
     url=pagina
-    ##status true
+
     print("\nENLACES EXTRAIDOS DE LA PAGINA WEB: " + pag_inicial + "\n")
     bs = BeautifulSoup(url.read(), 'html.parser')
-    for enlaces in bs.find_all("a"): 
-        pag=enlaces.get("href")
-        b=False
-        ##insertar en la tabla
+    lista_enlaces=bs.find_all("a")
+    for enlaces in lista_enlaces :
+        for i in lista_enlaces:
+            try:
+                url: str=i["href"]
+            except KeyError:
+                continue
+            if not url.startswith("http"):
+                continue
+            try:
+                operacion.execute(f'INSERT INTO web VALUES ("{url}",false)')
+            except mysql.errors.IntegrityError:
+                continue
+    
+
         print("href: {}".format(enlaces.get("href")))
 print("\nFIN DE ENLACES ENCONTRADOS EN:  "+pag_inicial+"\n")
 
 
 conexion.close()
 
-
+'''
 

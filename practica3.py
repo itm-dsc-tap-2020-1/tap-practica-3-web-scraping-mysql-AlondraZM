c119cd1a402f43008871117251bbaae785afac1e
AlondraZM <61289225+AlondraZM@users.noreply.github.com>
2020-03-11 21:16:07 -0600

    Add files via upload

--- practica3.py
+++ practica3.py
@@ -4,20 +4,42 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import mysql.connector as mysql
 
-#funcional
-pag_inicial=input('INGRESE URL:  ')
-
-url = urlopen(pag_inicial)
-print("\nENLACES EXTRAIDOS DE LA PAGINA WEB: " + pag_inicial + "\n")
-bs = BeautifulSoup(url.read(), 'html.parser')
-for enlaces in bs.find_all("a"):
-    print("href: {}".format(enlaces.get("href")))
-print("\nFIN DE ENLACES ENCONTRADOS EN:  "+pag_inicial+"\n")
 
 conexion = mysql.connect( host='localhost', user= 'alondra', passwd='garu', db='practica3' )
 operacion = conexion.cursor()
 operacion.execute( "SELECT * FROM web" )
+
+pag_inicial=input('INGRESE URL:  ')
+url = urlopen(pag_inicial)
+
+
+print("\nENLACES EXTRAIDOS DE LA PAGINA WEB: " + pag_inicial + "\n")
+bs = BeautifulSoup(url.read(), 'html.parser')
+for enlaces in bs.find_all("a"):
+    pag=enlaces.get("href")
+    b=False
+    ##insertar en la tabla
+    print("href: {}".format(enlaces.get("href")))
+print("\nFIN DE ENLACES ENCONTRADOS EN:  "+pag_inicial+"\n")
+#mostrar tabla
 for pagina,status,  in operacion.fetchall() :
     print (pagina,status)
+
+
+for pagina,status,  in operacion.fetchall():
+    url=pagina
+    ##status true
+    print("\nENLACES EXTRAIDOS DE LA PAGINA WEB: " + pag_inicial + "\n")
+    bs = BeautifulSoup(url.read(), 'html.parser')
+    for enlaces in bs.find_all("a"): 
+        pag=enlaces.get("href")
+        b=False
+        ##insertar en la tabla
+        print("href: {}".format(enlaces.get("href")))
+print("\nFIN DE ENLACES ENCONTRADOS EN:  "+pag_inicial+"\n")
+
+
 conexion.close()
+
+
 
